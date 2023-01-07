d802c4ac1447c3416ea6fad74b6c39946eb606bf
daudujohn <daudujoe@gmail.com>
2023-01-07 15:39:35 +0100

    Initial commit

---
+++ HackerRank 30 days of code/Day 7 Arrays.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    for num in arr[::-1]:
+        print(num, end=" ")
+    
+    
